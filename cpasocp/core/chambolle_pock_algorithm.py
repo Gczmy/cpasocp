08b41bdb7514392e063d233df9907cef1014f93d
Gczmy <657968310@qq.com>
2022-05-11 15:32:01 +0100

    fixed while loop condition

--- cpasocp/core/chambolle_pock_algorithm.py
+++ cpasocp/core/chambolle_pock_algorithm.py
@@ -34,6 +34,7 @@ def chambolle_pock_algorithm_for_ocp(epsilon, initial_guess_z, initial_guess_eta
     C_N = terminal_ncc_set_constraints
     n_x = A.shape[1]
     n_u = B.shape[1]
+    n_z = N * (n_x + n_u) + n_x
     n_Phi = Phi_z.shape[0]
     x0 = initial_state
     z0 = initial_guess_z
@@ -44,15 +45,16 @@ def chambolle_pock_algorithm_for_ocp(epsilon, initial_guess_z, initial_guess_eta
         raise ValueError("Initial guess vector eta row is not correct")
 
     # Choose α1, α2 > 0 such that α1α2∥Phi∥^2 < 1
-    alpha_1 = 0.99 / np.linalg.norm(Phi_z)
-    alpha_2 = 0.99 / np.linalg.norm(Phi_z)
+    alpha_1 = 0.99 / np.linalg.norm(Phi @ np.eye(n_z) * 0.5)
+    alpha_2 = 0.99 / np.linalg.norm(Phi @ np.eye(n_z) * 0.5)
 
-    z_prev = z0
     z_next = z0
-    eta_prev = eta0
     eta_next = eta0
 
-    while (np.abs(z_next - z_prev) < epsilon).all() and (np.abs(eta_next - eta_prev) < epsilon).all():
+    z_proximal_end = [False] * n_z
+    eta_proximal_end = [False] * n_Phi
+
+    while (all(z_proximal_end) and all(eta_proximal_end)) is False:
         z_prev = z_next
         eta_prev = eta_next
         z_next = core_online.proximal_of_h_online_part(prediction_horizon=prediction_horizon,
@@ -67,7 +69,12 @@ def chambolle_pock_algorithm_for_ocp(epsilon, initial_guess_z, initial_guess_eta
                                                        K_seq=K_seq,
                                                        A_bar_seq=A_bar_seq)
         eta_half_next = eta_prev + alpha_2 * Phi @ (2 * z_next - z_prev)
-        eta_next = eta_half_next - proj_c(C_t, C_N, alpha_2 * (1 / alpha_2 * eta_half_next), prediction_horizon,
-                                          stage_state_constraints, terminal_state_constraints)
-
+        eta_next = eta_half_next - alpha_2 * proj_c(C_t, C_N, 1 / alpha_2 * eta_half_next, prediction_horizon,
+                                                    stage_state_constraints, terminal_state_constraints)
+        for i in range(n_z):
+            if np.abs(z_next[i] - z_prev[i]) < epsilon:
+                z_proximal_end[i] = True
+        for i in range(n_Phi):
+            if np.abs(eta_next[i] - eta_prev[i]) < epsilon:
+                eta_proximal_end[i] = True
     return z_next, eta_next
